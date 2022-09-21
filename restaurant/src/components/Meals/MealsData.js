2ecd90728ad2c94f7827cfb2fa7f3523d8ac21ea
MilinkovicZ <zdravkolegenda123@gmail.com>
2022-09-21 21:49:41 +0200

    Bug Fixing

--- restaurant/src/components/Meals/MealsData.js
+++ restaurant/src/components/Meals/MealsData.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 import Card from "../UI/Card.js";
-import MealItem from "./MealItem.js";
+import MealItem from "./MealItem/MealItem.js";
 import styles from "./MealsData.module.css";
 
 const MEALS_INITIAL = [
   {
     id: "m1",
     name: "Sushi",
-    description: "Finest fish and veggies",
+    description: "Finest fish from Japan!",
     price: 22.99,
   },
   {
     id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
+    name: "Karadjordjeva Schnitzel",
+    description: "A serbian specialty!",
     price: 16.5,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
-    description: "American, raw, meaty",
+    description: "American, extra cheese, hot!",
     price: 12.99,
   },
   {
     id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
+    name: "Vegetarian dish",
+    description: "Very green and healthy!",
     price: 18.99,
   },
 ];
@@ -37,6 +37,7 @@ function MealsData() {
       <ul>
         {meals.map((meal) => (
           <MealItem
+            key={meal.id}
             name={meal.name}
             description={meal.description}
             price={meal.price}
